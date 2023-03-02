ca54a72ad544b347d86984b1704efe514cb211c3
AkssharRamesh03 <rmshakshar@gmail.com>
2023-03-02 16:48:54 +0000

    backend project structure

---
+++ src/test/java/com/hospital/sterileService/SterileServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hospital.sterileService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SterileServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
