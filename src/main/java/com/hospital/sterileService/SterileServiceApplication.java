a1356c3b5258aa9e09655fafcce3be4e55cc1bdc
AkssharRamesh03 <rmshakshar@gmail.com>
2023-03-02 16:48:54 +0000

    backend project structure

---
+++ src/main/java/com/hospital/sterileService/SterileServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hospital.sterileService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SterileServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SterileServiceApplication.class, args);
+    }
+
+}
