a95e74d4858d092a5c39d24b82bed4acc9a3e496
martaignatowicz <msmartaignatowicz@gmail.com>
2023-03-17 12:38:49 +0000

    changed backend url

--- src/Url/url.js
+++ src/Url/url.js
@@ -1,7 +1,2 @@
-
-import axios from "axios";
-export const Base_url = 'http://localhost:8080';
-export const myAxios =  axios.create({
-    baseURL: Base_url,
-
-});
+export const Base_url = "http://localhost:8080/api";
+export default Base_url;
