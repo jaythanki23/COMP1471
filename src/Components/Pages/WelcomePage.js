33bc34687e7a6f5b1a4bb9bbf95cb8ac5c9fe771
martaignatowicz <msmartaignatowicz@gmail.com>
2023-03-17 12:25:12 +0000

    additional pages and changes

---
+++ src/Components/Pages/WelcomePage.js
@@ -0,0 +1,15 @@
+import '../../App.css';
+import WelcomeSection from '../WelcomeSection';
+import React from "react";
+import Cards from "../Cards";
+
+function WelcomePage() {
+    return (
+        <>
+            <WelcomeSection />
+            <Cards />
+        </>
+    );
+}
+
+export default WelcomePage;
