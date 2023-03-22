464bfc16da452d9b153a883f1d08bd84b2d99e12
Tumsyy <marcel.h.w.pf@web.de>
2023-03-22 20:27:32 +0000

    CrossOrigin annotations

--- src/main/java/com/hospital/sterileService/Controller/SterilisationProcessController.java
+++ src/main/java/com/hospital/sterileService/Controller/SterilisationProcessController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/sterilisation_process/")
 public class SterilisationProcessController {
--- src/main/java/com/hospital/sterileService/Controller/TrayConfigurationController.java
+++ src/main/java/com/hospital/sterileService/Controller/TrayConfigurationController.java
@@ -13,6 +13,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/configuration")
 public class TrayConfigurationController {
