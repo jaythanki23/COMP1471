bd763fd2b6c970520b03db86df4b9957a45cfd60
martaignatowicz <msmartaignatowicz@gmail.com>
2023-03-25 18:59:13 +0000

    changed styling of tracking button

--- src/Components/Pages/HospitalStaff.js
+++ src/Components/Pages/HospitalStaff.js
@@ -2,27 +2,19 @@ import '../../App.css';
 import OrderForm from "../OrderForm";
 import React from "react";
 import '../OrderForm.css'
-import {Link} from "react-router-dom";
-import styled from "styled-components";
-
-const StyledLink = styled(Link)`
-  color: Blue;
-  text-decoration: none;
-  margin: 1rem;
-  margin-left: 17.5rem;
-  margin-top: 3rem;
-  position: relative;
-  font: sans-serif;
-`;
+import './HospitalStaff.css'
 
 function HospitalStaff() {
     return (
         <>
-            <StyledLink
-                className="trackingLink"
-                to={'/tracking'}>
-                    Track your order
-            </StyledLink>
+            <button
+                type="button"
+                className="button"
+                onClick={(e) => {
+                    e.preventDefault();
+                    window.location.href='http://localhost:3000/tracking';
+                }}
+            > Track your order</button>
             <OrderForm />
         </>
     );
