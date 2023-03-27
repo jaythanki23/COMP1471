863a7fe743b2b651b0da9c8f7ad7871a59caa44e
Marta Ignatowicz <73638941+martaignatowicz@users.noreply.github.com>
2023-03-27 14:48:38 +0100

    Update ManageChoice.js

--- src/Components/Pages/ManageChoice.js
+++ src/Components/Pages/ManageChoice.js
@@ -11,9 +11,17 @@ export default function ManageChoice() {
                 className="button"
                 onClick={(e) => {
                     e.preventDefault();
-                    window.location.href='http://localhost:3000/manager/manage/trays';
+                    window.location.href='http://localhost:3000/manager/manage/configs';
                 }}
             > Manage tray configurations</button>
+            <button
+                type="button"
+                className="button"
+                onClick={(e) => {
+                    e.preventDefault();
+                    window.location.href='http://localhost:3000/manager/manage/instruments';
+                }}
+            > Manage sterilization instrument types</button>
             <button
                 type="button"
                 className="button"
