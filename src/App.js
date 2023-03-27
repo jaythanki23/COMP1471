d513bf2f038e15631cdd670c321c59639c15316a
Marta Ignatowicz <73638941+martaignatowicz@users.noreply.github.com>
2023-03-27 21:13:36 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,22 +9,34 @@ import Manager from './Components/Pages/Manager';
 import NavBar from "./Components/NavBar";
 import Tracking from "./Components/Pages/Tracking";
 import ViewOrder from "./Components/ViewOrder";
+import EditOrder from "./Components/Pages/EditOrder";
+import ManageChoice from "./Components/Pages/ManageChoice";
+import ManageTrayConfigs from './Components/Pages/ManageTrayConfigs';
+import ManageSteps from "./Components/Pages/ManageSteps";
+import ViewTrayConfig from './Components/Pages/ViewTrayConfig';
+import ManageInstrumentTypes from './Components/Pages/ManageInstrumentTypes'
 
 function App() {
-  return (
-      <Router>
-          <ScrollToTop />
-          <NavBar />
-          <Routes>
-          <Route path='/' exact element={<WelcomePage />} />
-              <Route path='/hospital-staff' exact element={<HospitalStaff />} />
-              <Route path='/sterilization-staff' exact element={<SterilizationStaff />} />
-              <Route path='/manager' exact element={<Manager />} />
-              <Route path='/tracking' exact element={<Tracking />} />
-              <Route path='/tracking/view-order/:id' exact element={<ViewOrder/>} />
-          </Routes>
-      </Router>
-  );
+    return (
+        <Router>
+            <ScrollToTop />
+            <NavBar />
+            <Routes>
+                <Route path='/' exact element={<WelcomePage />} />
+                <Route path='/hospital-staff' exact element={<HospitalStaff />} />
+                <Route path='/sterilization-staff' exact element={<SterilizationStaff />} />
+                <Route path='/manager' exact element={<Manager />} />
+                <Route path='/tracking' exact element={<Tracking />} />
+                <Route path='/tracking/view-order/:id' exact element={<ViewOrder/>} />
+                <Route path='/manager/edit-order/:id' element={<EditOrder/>} />
+                <Route path='/manager/manage' element={<ManageChoice/>} />
+                <Route path='/manager/manage/configs' element={<ManageTrayConfigs/>} />
+                <Route path='/manager/manage/steps' element={<ManageSteps/>} />
+                <Route path='/manager/manage/configs/:id' element={<ViewTrayConfig/>} />
+                <Route path='/manager/manage/instruments' element={<ManageInstrumentTypes/>} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
