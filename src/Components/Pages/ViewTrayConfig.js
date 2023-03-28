27d369fd3f02f7359353aa76ad53e5f2b3ae89ce
Marta Ignatowicz <73638941+martaignatowicz@users.noreply.github.com>
2023-03-28 16:34:38 +0100

    Update ViewTrayConfig.js

--- src/Components/Pages/ViewTrayConfig.js
+++ src/Components/Pages/ViewTrayConfig.js
@@ -1,96 +1,79 @@
 import axios from "axios";
 import React, { useEffect,useState } from "react";
 import {Link, useNavigate, useParams} from "react-router-dom";
-import {InstrumentApiClient} from "../../api/InstrumentApiClient";
-import {TrayApiClient} from "../../api/TrayApiClient";
 import InstrumentDetails from "../InstrumentDetails";
-import {SterilizationProcessApiClient} from "../../api/SterilizationProcessApiClient";
-import TrayType from "../TrayType";
-import TrayDetails from "../TrayDetails";
+import {InstrumentApiClient} from "../../api/InstrumentApiClient";
 
-export default function ViewTrayConfig({tray,role}) {
+export default function ViewTrayConfig() {
 
     let navigate = useNavigate();
+    const [instC, setInstC] = useState([])
 
-    const [trayConfiguration, setTrayConfiguration] = useState([]);
-    const [instrumentType, setInstrumentType] = useState([]);
-    const [instrumentC, setInstrumentC] = useState([]);
+    const [trayConfig, setTrayConfig] = useState({
+        id: "",
+        trayName: ""
+    });
+
+    const [instruments, setInstruments] = useState([]
+    )
 
     const { id } = useParams();
 
     useEffect(() => {
-        loadTrayConfiguration();
+        loadTrayConfig();
     }, []);
-
     useEffect(() => {
-        loadInstrumentType();
-    },[]);
-
+        loadInstruments()
+    })
     useEffect(() => {
-        loadInstrumentC();
-    }, []);
+        InstrumentApiClient.getInstrumentCountsForConfigId(id).then(
+            o=>setInstC(o));
+    }, [id]);
 
 
-    const loadTrayConfiguration = async () => {
-        const result = await axios.get(`http://localhost:8080/api/configuration/${id}`);
-        setTrayConfiguration(result.data);
-    };
-
-    const loadInstrumentType = async () => {
-        const result = await axios.get(`http://localhost:8080/api/instrument_type/${id}`);
-        setInstrumentType(result.data);
-    };
-
-    const loadInstrumentC = async () => {
+    const loadInstruments = async () => {
         const result = await axios.get(`http://localhost:8080/api/instrument_count/tray_config/${id}`);
-        setInstrumentC(result.data);
+        setInstruments(result.data);
+    }
+    const loadTrayConfig = async () => {
+        const result = await axios.get(`http://localhost:8080/api/configuration/${id}`);
+        setTrayConfig(result.data);
     };
 
-    useEffect(() => {
-        if(trayConfiguration == null)
-            return;
-
-        InstrumentApiClient.getInstrumentCountsForConfigId(trayConfiguration.id).then(
-            o=>setInstrumentC((o==undefined)?[]:o));
-    }, [trayConfiguration]);
+    const deleteTrayConfig = async (id) => {
+        await axios.delete(`http://localhost:8080/api/configuration/${id}`);
+        navigate("/manager/manage/configs");
+    };
 
     return (
         <div className="container">
             <div className="row">
                 <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow">
-                    <h2 className="header">Tray Details</h2>
+                    <h2 className="header">Order Details</h2>
                     <div className="card">
-                            <div className="list-group-title">
-                                Details of tray : {trayConfiguration.id}
-                                <ul className="list-group">
-                                    <li className="list-group-item">
-                                        <b>Tray configuration name: </b>
-                                        {trayConfiguration.trayName}
-                                    </li>
-                                    <li className="list-group-item">
-                                        <b>Instruments: </b>
-                                            <thead>
-                                            <tr>
-                                            <th scope="col">Name</th>
-                                            <th scope="col">Count</th>
-                                            <th scope="col">Sterilization Step</th>
-                                            </tr>
-                                            </thead>
-                                            <tbody>
-                                            {instrumentC.map((instC, index) => (
-                                                <tr>
-                                                <td>{instC.instrumentType.instrumentName}</td>
-                                                <td>{instC.instrumentCount}</td>
-                                                <td>{instC.instrumentType.step.stepName}</td>
-                                            </tr>
-                                            ))}
-                                            </tbody>
-                                    </li>
-                                </ul>
-                            </div>
+                        <div className="list-group-title">
+                            Details of tray configuration : {trayConfig.id}
+                            <ul className="list-group">
+                                <li className="list-group-item">
+                                    <b>Tray name: </b>
+                                    {trayConfig.trayName}
+                                </li>
+                                <li className="list-group-item">
+                                    <b>Instrument count: </b>
+                                    {instC.map((ic,index) => (
+                                        <InstrumentDetails key={index} inst={ic.instrumentType} count={ic.instrumentCount}/>
+                                    ))}
+                                </li>
+                                <button
+                                    className="button" onClick={() => deleteTrayConfig(trayConfig.id)}
+                                >
+                                    Delete
+                                </button>
+                            </ul>
+                        </div>
                     </div>
                     <Link className="btn btn-primary my-2" to={"/manager/manage/configs"}>
-                        Back to Configs
+                        Back to tray configurations
                     </Link>
                 </div>
             </div>
